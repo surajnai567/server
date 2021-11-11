7bc21996f8c72dabef841c50f619b9cb5676aeac
surajnai567 <surajnai567@gmail.com>
2021-11-11 19:26:32 +0530

    Initial commit

---
+++ client.js
@@ -0,0 +1,10 @@
+const io = require("socket.io-client");
+//First Connect to the Server on the Specific URL (HOST:PORT)
+let socket = io("http://localhost:3000");
+//Now Listen for Events (welcome event).
+socket.on("welcome", (data) => {
+   /*For the listener we specify the event name and we give the callback to which be called one the
+   event is emitted*/
+   //Log the Welcome message
+   console.log("Message: ", data);
+});
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const http = require("http").createServer();
+const io = require("socket.io")(http);
+//Listen for a client connection
+//Listen the HTTP Server
+http.listen(port, () => {
+    console.log("Server Is Running Port: " + port);
+});
+
+io.on("connection", (socket) => {
+    //Socket is a Link to the Client
+    console.log("New Client is Connected!", socket.id);
+    //Here the client is connected and we can exchanged
+    //Send Message
+    //We need to use the Socket (the link between the server and the connected user(s)).
+    socket.emit("welcome", "Hello and Welcome to the Server");
+    socket.on('w', (msg) => {
+    console.log('message: ' + msg);
+  });
+});

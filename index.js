f57f7fc84acd959eebe823f6aad8c61e51d07be9
surajnai567 <surajnai567@gmail.com>
2021-11-11 19:50:09 +0530

    start

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 80;
 const http = require("http").createServer();
 const io = require("socket.io")(http);
 //Listen for a client connection
